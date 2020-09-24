b9f3d842c88777fb193d7e30f1e1d64dc6a755ca
guillaume-ruer <52717105+guillaume-ruer@users.noreply.github.com>
2020-09-24 08:52:29 +0200

    Ajout de helloController

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,2 +1,18 @@
-<?php
-//Hello Controller
+<?php 
+// src/Controller/HelloController.php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController extends AbstractController {
+   /**
+     * @Route("/hello/{name}", name="hello")
+    */
+    public function indexAction($name) {
+        if(!ctype_alpha($name)){
+            throw new \Exception("Ce n'est pas alphabétique.");
+        }
+        return new Response('<html><body>Hello ' . $name . '!</body></html>');
+     }
+}
